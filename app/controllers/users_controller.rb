4bb9fa6eabfeff8e316452afbce03ea80e51f6b1
KouOsada <kouosada.0930@gmail.com>
2021-10-08 08:56:42 +0000

    [Add]user詳細画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,21 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def index
     @users = User.all
     @book = Book.new
   end
-  
-  def create
-  end
 
   def edit
   end
-  
+
   def update
   end
-  
+
 end
