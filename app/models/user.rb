971f9c31c3d83067ec75380a0a62c231dd5e1da6
KouOsada <kouosada.0930@gmail.com>
2021-10-08 10:30:24 +0000

    [Add]コメント機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  
+  # アソシエーション
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   
+  # バリデーション
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
   validates :introduction, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: 'home_about'
   resources :books do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:show, :index, :edit, :update]
 end
