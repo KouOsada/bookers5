609b81e108a9a03b973ecb9b576acc14dbd80b27
KouOsada <kouosada.0930@gmail.com>
2021-10-08 08:26:09 +0000

    [Add]投稿詳細画面・投稿削除アクション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,10 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  
+  before_action :ensure_correct_user, only: [:edit, :update, :destroy]
+
   def show
+    @book = Book.find(params[:id])
+    @book_new = Book.new
   end
 
   def index
@@ -21,25 +24,35 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully"
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
   end
-  
+
   private
-  
+
   def book_params
     params.require(:book).permit(:title, :body)
   end
-  
+
   def ensure_correct_user
     @book = Book.find(params[:id])
     unless @book.user == current_user
       redirect_to books_path
     end
   end
-  
+
 end
