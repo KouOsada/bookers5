0532bc55a528e6ab2341f44cdb13bf1f9f280a48
KouOsada <kouosada.0930@gmail.com>
2021-10-09 06:48:57 +0000

    [Add]フォロー機能のリレーション

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  # アソシエーション
+  # リレーション
   has_many :books, dependent: :destroy
   attachment :profile_image, destroy: false
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  # フォロー機能
+  # [被フォロー]
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # [与フォロー]
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  # [被フォローを通じて参照→自分をフォローしている人]
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  # [与フォローを通じて参照→自分がフォローしている人]
+  has_many :followings, through: :relationships, source: :followed
   
   # バリデーション
   validates :name, length: {maximum: 20, minimum: 2}, uniqueness: true
