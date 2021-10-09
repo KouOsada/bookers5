b31bb70f8aa5ba7bd8c15a4312d0275821ad14c2
KouOsada <kouosada.0930@gmail.com>
2021-10-09 07:37:19 +0000

    [Add]follow機能 to users/index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_072509) do
+ActiveRecord::Schema.define(version: 2021_10_09_062518) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -36,10 +36,10 @@ ActiveRecord::Schema.define(version: 2021_10_09_072509) do
   end
 
   create_table "relationships", force: :cascade do |t|
+    t.integer "follwer_id"
     t.integer "followed_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "follower_id"
   end
 
   create_table "users", force: :cascade do |t|
