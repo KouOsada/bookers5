0901ca6426a09876dba787e8f0cc70d4ac82028a
KouOsada <kouosada.0930@gmail.com>
2021-10-13 08:15:41 +0000

    [Add]チャット機能に必要なモデル

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_09_062518) do
+ActiveRecord::Schema.define(version: 2021_10_13_081456) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -28,6 +28,14 @@ ActiveRecord::Schema.define(version: 2021_10_09_062518) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "chats", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "favorites", force: :cascade do |t|
     t.integer "user_id"
     t.integer "book_id"
@@ -36,8 +44,20 @@ ActiveRecord::Schema.define(version: 2021_10_09_062518) do
   end
 
   create_table "relationships", force: :cascade do |t|
-    t.integer "follwer_id"
     t.integer "followed_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "follower_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_rooms", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "room_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
